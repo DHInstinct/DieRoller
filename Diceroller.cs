01c6e4571904a71281103645a670f7439e19b930
Daniel Hartley <danielhartley117@gmail.com>
2020-10-07 17:21:43 -0400

    Add files via upload

---
+++ Diceroller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diceroller
+{
+    class Diceroller
+    {
+
+        public void Roll() // rolling 1D6
+        {
+            Console.WriteLine(StaticRandom.Instance.Next(1, 7));
+        }
+        public void Roll(int times = 0)
+        {
+            int[] total = new int[times];
+            for(int i = 0; i < times; ++i)
+            {
+                total[i] += StaticRandom.Instance.Next(1, 7);
+                Console.WriteLine(total[i]);
+            }
+        }
+        public void Roll(int times = 0, int sides = 0)
+        {
+            int[] total = new int[times];
+            for (int i = 0; i < times; ++i)
+            {
+                total[i] += StaticRandom.Instance.Next(1, sides);
+                Console.WriteLine(total[i]);
+            }
+        }
+        public void Roll(int times = 0, int sides = 0, int target = 0)
+        {
+            int counter = 0;
+            int[] total = new int[times];
+            for (int i = 0; i < times; ++i)
+            {
+                total[i] += StaticRandom.Instance.Next(1, sides);
+                if(total[i] >= target)
+                    counter++;
+
+            }
+                Console.WriteLine(counter);
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diceroller
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Diceroller diceroller = new Diceroller();
+
+           Console.WriteLine("Rolling D6:");
+           diceroller.Roll();
+           Console.WriteLine("Rolling D6 10 times:");
+           diceroller.Roll(10);
+           Console.WriteLine("Rolling D10 10 times:");
+           diceroller.Roll(10,10);
+           Console.WriteLine("Rolling D10 10 times with a target of 5:");
+           diceroller.Roll(10, 10, 5);
+
+
+
+
+
+
+        }
+    }
+}
